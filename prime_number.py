0c735c964e089644725ae5214fba1f3895513749
nyanyans2 <nyanyan.choki@gmail.com>
2024-05-06 09:49:57 +0900

    制御構文の解答

--- prime_number.py
+++ prime_number.py
@@ -1,4 +1,21 @@
-a = input("aの値を入力: ")
-b = input("bの値を入力: ")
+a = 61
+b = 10
 
 # TODO
+
+
+for i in range(2,a):
+    if a % i == 0:
+        print("aは素数ではありません")
+        break
+    else:
+        print("aは素数です")
+
+for i in range(2,b):
+    if b % i == 0:
+        print("bは素数ではありません")
+        break
+    else:
+        print("bは素数です")
+
+    
--- trapezoidal_integral.py
+++ trapezoidal_integral.py
@@ -3,3 +3,26 @@ from math import sin
 # print(sin(0))
 # >>> 0
 # -----------
+
+
+a= 0
+b= 1
+N= 100
+
+h = ( b - a ) / N
+
+daikei_s = 0
+
+for k in range(1, N):
+    x_l = a+(k-1)*h
+    x_r = a+k*h
+    y_l = sin(x_l)
+    y_r = sin(x_r)
+    daikei_s += (y_l + y_r) / 2
+
+
+sekibun = h * daikei_s
+
+
+
+print("台形積分の近似値",sekibun)
