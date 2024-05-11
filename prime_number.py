785c3f3f100374284d4464ac6150d51cccb9b953
nyanyans2 <nyanyan.choki@gmail.com>
2024-05-11 10:05:59 +0900

    制御構文直し２

--- prime_number.py
+++ prime_number.py
@@ -3,24 +3,33 @@ b = 10
 
 # TODO
 
-prime_a = True
+if a == 1:
+   prime_a = False
 
-for i in range(2,a):
-    if a % i == 0:
-        prime_a = False
-        break
+else:
+    prime_a = True
+
+    for i in range(2,a):
+            if a % i == 0:
+                prime_a = False
+            break
     
 if prime_a:
         print("aは素数です")
 else:
         print("aは素数ではありません")
 
-prime_b = True
 
-for i in range(2,b):
-    if b % i == 0:
-        prime_b = False
-        break
+if b == 1:
+   prime_b= False
+
+else:
+    prime_b = True
+
+    for i in range(2,b):
+            if a % i == 0:
+                prime_b = False
+            break
     
 if prime_b:
         print("bは素数です")
